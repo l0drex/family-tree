a593bd5f8315027b5c0cb03d58b25f98b763fdc6
l0drex <hoffmann_lorenz@protonmail.com>
2022-01-09 18:46:14 +0100

    Links to etc nodes are now diagonal

--- js/tree.js
+++ js/tree.js
@@ -391,9 +391,11 @@ function update() {
     link
       .attr("points", d => {
         if (d.target.type === "family")
-          return d.source.x + "," + d.source.y + " " + d.source.x + "," + (d.target.y - personDiff) + " " + d.target.x + "," + (d.target.y - personDiff);
+          return `${d.source.x},${d.source.y} ${d.source.x},${d.target.y - personDiff} ${d.target.x},${d.target.y - personDiff}`;
+        else if ([d.source.type, d.target.type].includes("etc"))
+          return `${d.source.x},${d.source.y} ${d.target.x},${d.target.y}`
         else
-          return d.source.x + "," + (d.source.y + personDiff) + " " + d.target.x + "," + (d.source.y + personDiff) + " " + d.target.x + "," + d.target.y;
+          return `${d.source.x},${d.source.y + personDiff} ${d.target.x},${d.source.y + personDiff} ${d.target.x},${d.target.y}`;
       });
   });
 }
