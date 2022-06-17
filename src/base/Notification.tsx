01cd4312dbf8d3b8ee22d6edfb45ffa8666352a1
l0drex <hoffmann_lorenz@protonmail.com>
2022-06-17 16:56:29 +0200

    Fix styling of notifications

--- src/base/Notification.tsx
+++ src/base/Notification.tsx
@@ -1,9 +1,14 @@
+import {translationToString} from "../main";
+import "./Notification.css";
+
 function Notification(props) {
     if (props.type === "warning") {
         return (
             <div className="notification warning" data-reason={props.reason}>
-                <b lang="en"><span className="emoji">⚠</span>️ Warning</b>
-                <b lang="de"><span className="emoji">⚠</span>️ Achtung</b>
+                <b><span className="emoji">⚠</span>️ {translationToString({
+                  en: "Warning",
+                  de: "Achtung"
+                })} | </b>
                 <span className="description">{props.description}</span>
             </div>
         );
@@ -11,8 +16,10 @@ function Notification(props) {
     if (props.type === "error") {
         return (
             <div className="notification error" data-reason={props.reason}>
-                <b lang="en"><span className="emoji">💥</span> Error</b>
-                <b lang="de"><span className="emoji">💥</span> Fehler</b>
+                <b lang="en"><span className="emoji">💥</span> {translationToString({
+                  en: "Error",
+                  de: "Fehler"
+                })} | </b>
                 <span className="description">{props.description}</span>
             </div>
         );
