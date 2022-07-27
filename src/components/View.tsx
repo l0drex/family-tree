7b61307a87b248dee335df3c348b6e43324627ab
l0drex <l0drex@pm.me>
2022-07-27 23:48:03 +0200

    Removed unused import

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -4,7 +4,6 @@ import "./View.css";
 import {graphModel} from "../backend/ModelGraph";
 import {ViewMode, ViewGraph, ColorMode} from "../backend/ViewGraph";
 import TreeView from "./TreeView";
-import {GraphPerson} from "../backend/graph";
 import InfoPanel from "./InfoPanel";
 import * as React from "react";
 import FamilyPath from "./FamilyPath";
