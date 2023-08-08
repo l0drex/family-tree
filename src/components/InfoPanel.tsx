a8cca0346d1c32cbc39289494c542cf2fd4f0963
l0drex <l0drex@pm.me>
2023-08-08 16:23:25 +0200

    Change relationship view

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -1,8 +1,8 @@
-import {baseUri} from "../gedcomx/types";
-import {filterLang, strings} from "../main";
-import {db} from "../backend/db";
-import {useLiveQuery} from "dexie-react-hooks";
-import {Fact, GDate, Person} from "../gedcomx/gedcomx-js-extensions";
+import { baseUri } from "../gedcomx/types";
+import { filterLang, strings } from "../main";
+import { db } from "../backend/db";
+import { useLiveQuery } from "dexie-react-hooks";
+import { Fact, GDate, Person } from "../gedcomx/gedcomx-js-extensions";
 import {
   Attribution,
   ConclusionMisc, Notes, SourceReferences,
@@ -10,62 +10,13 @@ import {
   SubjectMisc,
   SubjectSidebar
 } from "./GedcomXComponents";
-import {Sidebar} from "../App";
+import { Sidebar } from "../App";
 import { Article, Details, PopupButton, Tag, Tags, Title } from "./GeneralComponents";
-import {Name} from "gedcomx-js";
+import { Name } from "gedcomx-js";
 import emojis from '../backend/emojies.json';
 import React, { Fragment } from "react";
 
 function InfoPanel({person}: { person: Person }) {
-  const parents = useLiveQuery(async () => {
-    if (!person) return;
-
-    return db.getParentsOf(person.id).then(parents =>
-      Promise.all(parents.map(r => db.personWithId(r))));
-  }, [person])
-
-  const children = useLiveQuery(async () => {
-    if (!person) return;
-
-    return db.getChildrenOf(person.id).then(children =>
-      Promise.all(children.map(r => db.personWithId(r))));
-  }, [person])
-
-  const partner = useLiveQuery(async () => {
-    if (!person) return;
-
-    return db.getPartnerOf(person.id).then(partner =>
-      Promise.all(partner.map(r => db.personWithId(r))));
-  }, [person])
-
-  const godparents = useLiveQuery(async () => {
-    if (!person) return;
-
-    return db.getGodparentsOf(person.id).then(parents =>
-      Promise.all(parents.map(r => db.personWithId(r))));
-  }, [person])
-
-  const godchildren = useLiveQuery(async () => {
-    if (!person) return;
-
-    return db.getGodchildrenOf(person.id).then(children =>
-      Promise.all(children.map(r => db.personWithId(r))));
-  }, [person])
-
-  const enslavedBy = useLiveQuery(async () => {
-    if (!person) return;
-
-    return db.getEnslavers(person.id).then(children =>
-      Promise.all(children.map(r => db.personWithId(r))));
-  }, [person])
-
-  const slaves = useLiveQuery(async () => {
-    if (!person) return;
-
-    return db.getSlaves(person.id).then(children =>
-      Promise.all(children.map(r => db.personWithId(r))));
-  }, [person])
-
   if (!person) {
     return <aside id={"info-panel"}></aside>
   }
@@ -76,22 +27,7 @@ function InfoPanel({person}: { person: Person }) {
 
       <Facts facts={person.getFacts()}/>
 
-      <Details title={strings.gedcomX.relationship.relationships}>
-        <RelationshipGroup relationships={parents} emoji={emojis.relationship.parent}
-                           title={strings.gedcomX.relationship.parents}/>
-        <RelationshipGroup relationships={children} emoji={emojis.relationship.child}
-                           title={strings.gedcomX.relationship.children}/>
-        <RelationshipGroup relationships={partner} emoji={emojis.relationship.partner}
-                           title={strings.gedcomX.relationship.partner}/>
-        <RelationshipGroup relationships={godparents} emoji={emojis.relationship.godparent}
-                           title={strings.gedcomX.relationship.godparents}/>
-        <RelationshipGroup relationships={godchildren} emoji={emojis.relationship.godchild}
-                           title={strings.gedcomX.relationship.godchildren}/>
-        <RelationshipGroup relationships={enslavedBy} emoji={emojis.relationship.enslaver}
-                           title={strings.gedcomX.relationship.enslavedBy}/>
-        <RelationshipGroup relationships={slaves} emoji={emojis.relationship.slaves}
-                           title={strings.gedcomX.relationship.slaves}/>
-      </Details>
+      <Relationships person={person}/>
 
       <SubjectArticles subject={person} noMargin/>
       <Tags>
@@ -152,40 +88,51 @@ function Facts({facts}: { facts: Fact[] }) {
     {facts
       .filter(filterLang)
       .map((f, i) => {
-        const hasMisc = f.getDate() || f.getPlace() || f.getQualifiers().length > 0
-          || f.confidence || f.analysis;
-
         return <Article key={i}>
-          <div className="flex flex-row gap-4">
-            <div className="flex-grow">
-              {f.emoji} {strings.gedcomX.person.factTypes[f.type.substring(baseUri.length)]
-              + (f.value ? `: ${f.value}` : "")}
-
-              {hasMisc && <section className="mt-2 flex flex-row flex-wrap gap-2">
-                {f.getDate() && <ArticleTag>{f.getDate().toString()}</ArticleTag>}
-                {f.getPlace() && <ArticleTag>{f.getPlace().toString()}</ArticleTag>}
-                {f.getQualifiers().map((q, i) =>
-                  <ArticleTag key={i}>
-                    {strings.gedcomX.factQualifier[q.name.substring(baseUri.length)]}: {q.value}
-                  </ArticleTag>)}
-                <ConclusionMisc conclusion={f} bgColor="bg-bg-light dark:bg-bg-dark"/>
-              </section>}
-            </div>
-            <div className="flex flex-col">
-              {f.notes && <PopupButton title={emojis.note}>
-                {<Notes notes={f.notes}/>}
-              </PopupButton>}
-              {f.attribution && <PopupButton title={emojis.attribution}>
-                <Attribution attribution={f.attribution}/>
-                ️</PopupButton>}
-              {f.sources && <PopupButton title={emojis.source.default}>
-                <SourceReferences references={f.sources}/>
-              </PopupButton>}
-            </div>
-          </div>
+          <FactContent fact={f} />
         </Article>
       })}
   </section>
+}
+
+function FactContent({fact}: {
+  fact: Fact
+}) {
+  const hasMisc = fact.getDate() || fact.getPlace() || fact.getQualifiers().length > 0
+    || fact.confidence || fact.analysis;
+
+  const originalType = fact.type.substring(baseUri.length);
+  const type = strings.gedcomX.person.factTypes[originalType]
+    ?? strings.gedcomX.relationship.factTypes.Couple[originalType]
+    ?? strings.gedcomX.relationship.factTypes.ParentChild[originalType]
+    ?? originalType;
+
+  return <div className="flex flex-row gap-4">
+    <div className="flex-grow">
+      {fact.emoji} {type + (fact.value ? `: ${fact.value}` : "")}
+
+      {hasMisc && <section className="mt-2 flex flex-row flex-wrap gap-2">
+        {fact.getDate() && <ArticleTag>{fact.getDate().toString()}</ArticleTag>}
+        {fact.getPlace() && <ArticleTag>{fact.getPlace().toString()}</ArticleTag>}
+        {fact.getQualifiers().map((q, i) =>
+          <ArticleTag key={i}>
+            {strings.gedcomX.factQualifier[q.name.substring(baseUri.length)]}: {q.value}
+          </ArticleTag>)}
+        <ConclusionMisc conclusion={fact} bgColor="bg-bg-light dark:bg-bg-dark"/>
+      </section>}
+    </div>
+    <div className="flex flex-col">
+      {fact.notes && <PopupButton title={emojis.note}>
+        {<Notes notes={fact.notes}/>}
+      </PopupButton>}
+      {fact.attribution && <PopupButton title={emojis.attribution}>
+        <Attribution attribution={fact.attribution}/>
+        ️</PopupButton>}
+      {fact.sources && <PopupButton title={emojis.source.default}>
+        <SourceReferences references={fact.sources}/>
+      </PopupButton>}
+    </div>
+  </div>;
 }
 
 function ArticleTag({children}) {
@@ -194,14 +141,89 @@ function ArticleTag({children}) {
   return <Tag bgColor="bg-bg-light dark:bg-bg-dark">{children}</Tag>
 }
 
-function RelationshipGroup({relationships, emoji, title}) {
-  if (!relationships || relationships.length === 0) return <></>
+function Relationships({person}: { person: Person }) {
+  const parents = useLiveQuery(async () => {
+    if (!person) return;
 
-  return <Article emoji={emoji} title={title}>
-    <ul>
-      {relationships.map(p => <li key={p.id}>{p.fullName}</li>)}
-    </ul>
-  </Article>
+    return db.getParentsOf(person.id).then(parents =>
+      Promise.all(parents.map(r => db.personWithId(r))));
+  }, [person])
+
+  const children = useLiveQuery(async () => {
+    if (!person) return;
+
+    return db.getChildrenOf(person.id).then(children =>
+      Promise.all(children.map(r => db.personWithId(r))));
+  }, [person])
+
+  const partner = useLiveQuery(async () => {
+    if (!person) return;
+
+    return db.getPartnerOf(person.id).then(partner =>
+      Promise.all(partner.map(r => db.personWithId(r))));
+  }, [person])
+
+  const godparents = useLiveQuery(async () => {
+    if (!person) return;
+
+    return db.getGodparentsOf(person.id).then(parents =>
+      Promise.all(parents.map(r => db.personWithId(r))));
+  }, [person])
+
+  const godchildren = useLiveQuery(async () => {
+    if (!person) return;
+
+    return db.getGodchildrenOf(person.id).then(children =>
+      Promise.all(children.map(r => db.personWithId(r))));
+  }, [person])
+
+  const enslavedBy = useLiveQuery(async () => {
+    if (!person) return;
+
+    return db.getEnslavers(person.id).then(children =>
+      Promise.all(children.map(r => db.personWithId(r))));
+  }, [person])
+
+  const slaves = useLiveQuery(async () => {
+    if (!person) return;
+
+    return db.getSlaves(person.id).then(children =>
+      Promise.all(children.map(r => db.personWithId(r))));
+  }, [person])
+
+  return <Details title={strings.gedcomX.relationship.relationships}>
+    <RelationshipType others={partner} emoji={emojis.relationship.partner}
+                      title={strings.gedcomX.relationship.partner} />
+    <RelationshipType others={parents} emoji={emojis.relationship.parent}
+                      title={strings.gedcomX.relationship.parents} />
+    <RelationshipType others={children} emoji={emojis.relationship.child}
+                      title={strings.gedcomX.relationship.children} />
+    <RelationshipType others={godparents} emoji={emojis.relationship.godparent}
+                      title={strings.gedcomX.relationship.godparents} />
+    <RelationshipType others={godchildren} emoji={emojis.relationship.godchild}
+                      title={strings.gedcomX.relationship.godchildren} />
+    <RelationshipType others={enslavedBy} emoji={emojis.relationship.enslaver}
+                      title={strings.gedcomX.relationship.enslavedBy} />
+    <RelationshipType others={slaves} emoji={emojis.relationship.slaves}
+                      title={strings.gedcomX.relationship.slaves} />
+  </Details>
+}
+
+function RelationshipType({others, emoji, title}: {
+  others: Person[],
+  emoji: string,
+  title: string
+}) {
+
+  if (!others?.length)
+    return <></>
+
+  return <section className="mb-4 last:mb-0 mt-2">
+    <Title emoji={emoji}>{title}</Title>
+    {others?.map((r, i) => <div key={i} className="mb-4 last:mb-0">
+      <Article>{others?.at(i)?.fullName}</Article>
+    </div>)}
+  </section>
 }
 
 export default InfoPanel;
